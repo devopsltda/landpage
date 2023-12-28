8e7902be377dfd0892e4bd6627e0a46f592d8c48
Pablo <pabloed0009@gmail.com>
2023-12-28 02:09:05 -0300

    :sparkles: responsividade completa  para mobile P, M, G

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.querySelectorAll(".smooth-scroll").forEach((anchor) => {
     });
   });
 });
+
+  // Aguarde o carregamento do DOM
+  document.addEventListener("DOMContentLoaded", function () {
+    // Selecione todos os links no nav
+    const navLinks = document.querySelectorAll('#main-nav a');
+
+    // Adicione um ouvinte de evento de clique a cada link
+    navLinks.forEach(function (link) {
+      link.addEventListener('click', function () {
+        // Encontre o checkbox de toggle-mobile
+        const toggleMobile = document.getElementById('toggle-mobile');
+
+        // Desmarque o checkbox para fechar a navegação móvel
+        toggleMobile.checked = false;
+      });
+    });
+  });
